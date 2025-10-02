6907ceb9c5ee0e43042e32f3ed8329cbaf6a2505
Oleg Usenko <oleg911usenko@gmail.com>
2025-10-02 13:31:08 +0300

    update oleg_weba_footbik,oleg_weba_footbik_system

--- oleg_weba_footbik/__manifest__.py
+++ oleg_weba_footbik/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "Oleg (Weba)",
     "website": "https://weba.com.ua/",
     "category": "Uncategorized",
-    "version": "17.0.11.2.0",
+    "version": "17.0.11.2.1",
     "license": "LGPL-3",
     "depends": ["contacts", "hr", "subscription_oca"],
     "data": [
